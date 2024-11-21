40b83c85d29ba2924b8d3be5a96d6805f9749365
Mirabell <elmirabegieva36@gmail.com>
2024-11-21 23:37:52 +0300

    Cycles_homework

---
+++ Homeworks_cycle.cs
@@ -0,0 +1,67 @@
+using System;
+
+class Program
+{
+// Задание 1
+//    static void Main()
+//    {
+//        //Инициализация первых двух чисел Фибоначчи
+//        int a = 0;
+//        int b = 1;
+//
+//        //Выводим первые 10 чисел Фибоначчи
+//        for (int i = 0; i < 10; i++)
+//        {
+//            Console.WriteLine(a);
+//            int znachenie = a;
+//            a = b;
+//            b = znachenie + b;
+//        }
+//    }
+
+// Задание 2
+//    static void Main()
+//    {
+//        //Выводим все чётные числа от 2 до 20
+//        for (int number = 2; number <= 20; number += 2)
+//        {
+//            Console.WriteLine(number);
+//        }
+//    }
+
+// Задание 3
+//    static void Main()
+//    {
+//        //Выводим таблицу умножения от 1 до 5
+//        for (int i = 1; i <= 5; i++)
+//        {
+//            for (int j = 1; j <= 10; j++)
+//            {
+//                Console.WriteLine(i + " * " + j + " = " + (i * j));
+//            }
+//            Console.WriteLine();
+//        }
+//    }
+
+// Задание 4
+//    static void Main()
+//    {
+//        string password = "qwerty";
+//        string userInput;
+//
+//        do
+//        {
+//            Console.WriteLine("Введите пароль:");
+//            userInput = Console.ReadLine();
+//
+//            if (userInput == password)
+//            {
+//                Console.WriteLine("Пароль верный!");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Пароль неверный! Попробуйте снова.");
+//            }
+//        } while (userInput != password);
+//    }
+}
