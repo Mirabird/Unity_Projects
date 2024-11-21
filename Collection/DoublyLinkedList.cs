469b1998ab2acba953e13e6ed389228926c9539b
Mirabell <elmirabegieva36@gmail.com>
2024-11-22 00:11:38 +0300

    Added Collections_homework

---
+++ Collection/DoublyLinkedList.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+namespace LinkedListExample
+{
+    class DoublyLinkedList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public void Add(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+
+            if (head == null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                tail.Next = newNode;
+                newNode.Previous = tail;
+                tail = newNode;
+            }
+        }
+
+        public void PrintForward()
+        {
+            Node<T> current = head;
+
+            Console.Write("Прямой порядок: ");
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Next;
+            }
+            Console.WriteLine();
+        }
+
+        public void PrintReverse()
+        {
+            Node<T> current = tail;
+
+            Console.Write("Обратный порядок: ");
+            while (current != null)
+            {
+                Console.Write(current.Data + " ");
+                current = current.Previous;
+            }
+            Console.WriteLine();
+        }
+    }
+
+    class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+        public Node<T> Previous { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+        }
+    }
+
+    class DoublyLinkedListTask
+    {
+        public void TaskLoop()
+        {
+            Console.WriteLine("Введите от 3 до 6 элементов для создания списка:");
+
+            int numberOfElements;
+            do
+            {
+                Console.Write("Количество элементов: ");
+                numberOfElements = Convert.ToInt32(Console.ReadLine());
+
+                if (numberOfElements >= 3 && numberOfElements <= 6)
+                    break;
+
+                Console.WriteLine("Некорректное количество элементов. Попробуйте снова или введите 'exit' для выхода.");
+
+                string input = Console.ReadLine();
+                if (input.ToLower() == "exit")
+                    return;
+
+            } while (true);
+
+            DoublyLinkedList<int> list = new DoublyLinkedList<int>();
+
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                Console.Write("Элемент " + (i + 1) + ": ");
+                int element = Convert.ToInt32(Console.ReadLine());
+                list.Add(element);
+            }
+
+            list.PrintForward();
+            list.PrintReverse();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Collection/Program.cs
@@ -0,0 +1,141 @@
+﻿using LinkedListExample;
+using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace Collection
+{
+    public class Task1
+    {
+        public void TaskLoop()
+        {
+            List<string> strings = new List<string>() { "строка1", "строка2", "строка3" };
+
+            Console.WriteLine("Список строк:");
+            PrintList(strings);
+
+            Console.Write("Введите новую строку: ");
+            string newString = Console.ReadLine();
+            if (newString == "exit")
+            {
+                Environment.Exit(0);
+            }
+
+            strings.Add(newString);
+
+            Console.WriteLine("Содержимое списка после добавления новой строки:");
+            PrintList(strings);
+
+            Console.Write("Введите ещё одну строку: ");
+            string additionalString = Console.ReadLine();
+
+            int index = strings.Count / 2;
+
+            strings.Insert(index, additionalString);
+
+            Console.WriteLine("Содержимое списка после добавления строки в середину:");
+            PrintList(strings);
+
+            Console.ReadLine();
+        }
+
+        private void PrintList(List<string> strings)
+        {
+            foreach (string str in strings)
+            {
+                Console.WriteLine(str);
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public class Task2
+    {
+        public void TaskLoop()
+        {
+            Dictionary<string, double> studentGrades = new Dictionary<string, double>();
+
+            while (true)
+            {
+                Console.WriteLine("Введите имя студента (или 'exit' для выхода):");
+                string name = Console.ReadLine();
+
+                if (name == "exit")
+                {
+                    break;
+                }
+
+                Console.WriteLine("Введите оценку студента (от 2 до 5):");
+                double grade;
+                bool isValidGrade = double.TryParse(Console.ReadLine(), out grade);
+
+                if (isValidGrade && grade >= 2 && grade <= 5)
+                {
+                    studentGrades[name] = grade;
+                    Console.WriteLine($"Оценка для студента {name} добавлена.");
+                }
+                else
+                {
+                    Console.WriteLine($"Некорректная оценка. Оценка должна быть от 2 до 5.");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Введите имя студента для получения его оценки (или 'q' для выхода):");
+            string studentName = Console.ReadLine();
+
+            while (studentName != "q")
+            {
+                if (studentGrades.ContainsKey(studentName))
+                {
+                    double grade = studentGrades[studentName];
+                    Console.WriteLine($"Оценка для студента {studentName}: {grade}");
+                }
+                else
+                {
+                    Console.WriteLine($"Студента с именем {studentName} не существует.");
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Введите имя студента для получения его оценки (или 'q' для выхода):");
+                studentName = Console.ReadLine();
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Выберите номер задачи:");
+            Console.WriteLine("1. Создать список строк и добавить в него элементы.");
+            Console.WriteLine("2. Создать словарь, связывая имена студентов с их средними оценками.");
+            Console.WriteLine("3. Написать реализацию двусвязного списка.");
+            Console.WriteLine("Выход - exit!");
+
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    Task1 task1 = new Task1();
+                    task1.TaskLoop();
+                    break;
+                case "2":
+                    Task2 task2 = new Task2();
+                    task2.TaskLoop();
+                    break;
+                case "3":
+                    DoublyLinkedListTask task = new DoublyLinkedListTask();
+                    task.TaskLoop();
+                    break;
+                case "exit":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Неверный номер задачи.");
+                    break;
+            }
+        }
+    }
+}
