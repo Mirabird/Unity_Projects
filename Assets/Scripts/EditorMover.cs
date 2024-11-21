870bba1c762174baa50e20f3b37bcc024071576b
Mirabell <elmirabegieva36@gmail.com>
2024-11-21 23:49:08 +0300

    Serialization-homework

---
+++ Assets/Scripts/EditorMover.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+	
+	[RequireComponent(typeof(PositionSaver))]
+	public class EditorMover : MonoBehaviour
+	{
+		private PositionSaver _save;
+		private float _currentDelay;
+
+		//todo comment: Что произойдёт, если _delay > _duration?
+		//Это может привести к тому, что задержка между сохранением позиций объекта будет дольше, чем общая длительность работы компонента
+		[SerializeField, Range(0.2f,1.0f)]        
+		private float _delay = 0.5f;
+		[SerializeField, Min(0.2f)]
+		private float _duration = 5f;
+
+		private void Start()
+		{
+            //todo comment: Почему этот поиск производится здесь, а не в начале метода Update?
+            //Для гарантии того, что он будет выполнен только один раз при старте объекта
+            _save = GetComponent<PositionSaver>();
+			_save.Records.Clear();
+
+			if (_duration <= _delay ) 
+			{
+				_duration = _delay * 5f;
+			}
+		}
+
+		private void Update()
+		{
+			_duration -= Time.deltaTime;
+			if (_duration <= 0f)
+			{
+				enabled = false;
+				Debug.Log($"<b>{name}</b> finished", this);
+				return;
+			}
+
+            //todo comment: Почему не написать (_delay -= Time.deltaTime;) по аналогии с полем _duration?
+            //Значение _duration постепенно уменьшается по мере прохождения времени, что позволяет отслеживать общую длительность работы компонента.
+			//В то время как _delay в данном случае задает интервал между сохранением позиций объекта
+            _currentDelay -= Time.deltaTime;
+			if (_currentDelay <= 0f)
+			{
+				_currentDelay = _delay;
+				_save.Records.Add(new PositionSaver.Data
+				{
+					Position = transform.position,
+                    //todo comment: Для чего сохраняется значение игрового времени?
+                    //Для воспроизведения и визуализации пути объекта во времени (?)
+                    Time = Time.time,
+				});
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/ReplayMover.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+	[RequireComponent(typeof(PositionSaver))]
+	public class ReplayMover : MonoBehaviour
+	{
+		private PositionSaver _save;
+
+		private int _index;
+		private PositionSaver.Data _prev;
+		private float _duration;
+
+		private void Start()
+		{
+            ////todo comment: зачем нужны эти проверки?
+            //Проверка, удалось ли найти компонент PositionSaver на текущем GameObject и имеет ли он записи. Если условие не выполняется, то выводится сообщение об ошибке в консоль
+            if (!TryGetComponent(out _save) || _save.Records.Count == 0)
+			{
+				Debug.LogError("Records incorrect value", this);
+                //todo comment: Для чего выключается этот компонент?
+                //Отключаем компонент ReplayMover в случае несоответствия условиям, чтобы остановить его работу
+                enabled = false;
+			}
+		}
+
+		private void Update()
+		{
+			var curr = _save.Records[_index];
+            //todo comment: Что проверяет это условие (с какой целью)? 
+            //Проверка, прошло ли уже достаточно времени для воспроизведения следующей записи из списка Record
+            if (Time.time > curr.Time)
+			{
+				_prev = curr;
+				_index++;
+                //todo comment: Для чего нужна эта проверка?
+                //Проверка, достигли ли мы конца списка записей. Если текущий индекс _index становится больше или равен количеству записей, то компонент отключается
+                if (_index >= _save.Records.Count)
+				{
+					enabled = false;
+					Debug.Log($"<b>{name}</b> finished", this);
+				}
+			}
+            //todo comment: Для чего производятся эти вычисления (как в дальнейшем они применяются)?
+            //Вычисление delta происходит для интерполяции двух позиций объекта между двумя кадрами записи
+            var delta = (Time.time - _prev.Time) / (curr.Time - _prev.Time);
+            //todo comment: Зачем нужна эта проверка?
+            //Проверка нужна для обработки случаев, когда данное значение delta является не числом. Если это происходит, то delta принимает значение 0f, чтобы избежать ошибок при вычислении позиции объекта
+            if (float.IsNaN(delta)) delta = 0f;
+            //todo comment: Опишите, что происходит в этой строчке так подробно, насколько это возможно
+            //В данной строке кода происходит интерполяция между предыдущей и текущей позициями объекта с использованием значения delta. Функция Vector3.Lerp выполняет
+			//линейную интерполяцию между двумя векторами с учетом коэффициента delta
+            transform.position = Vector3.Lerp(_prev.Position, curr.Position, delta);
+		}
+	}
+}
